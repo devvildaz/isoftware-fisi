fb2f237eff4be1f8865fbe1b65a3261f28e8edc4
devvildaz <diegovilca_dev@hotmail.com>
2022-05-13 01:15:08 -0500

    feat: label list

--- Entregable-01/ui/src/app.py
+++ Entregable-01/ui/src/app.py
@@ -1,20 +1,32 @@
 from kivy.app import App
 from kivy.uix.label import Label
 from kivy.uix.popup import Popup
+from kivy.uix.button import Button 
 from components.ImageButton import ImageButton
 from kivy.properties import ObjectProperty
-from core.Recognition import Recognition
+from kivy.properties import StringProperty 
+from core.Recognition import ProcessFoo 
 from kivy.factory import Factory 
+import json
 import os
 
 class ResultPopup(Popup):
+  photodir = StringProperty("")
+
+  def load_file(self):
+    response = ProcessFoo(self.photodir)
+    box = self.ids.resultbox
+    
+    for item in response:
+      box.add_widget(Button(size_hint_y=None,text=str(type(item['image']))))
+      for label in item['labels']:
+        box.add_widget(Button(text=str(type(label)), size_hint_y=None))
   pass
 
 class GuiApp(App):
   resultview = ObjectProperty()
   def __init__(self, **kwargs):
     super(GuiApp, self).__init__(**kwargs)
-    self.awsrecog = Recognition()
   def change_dir_event(self, dirpath):
     root = self.root
     stacklayout = root.ids.boxid.children[0]
@@ -33,7 +45,7 @@ class GuiApp(App):
         filepath=v
       )
       imgbtn.id = i
-      imgbtn.bind(on_release= lambda x: Factory.ResultPopup().open())
+      imgbtn.bind(on_release=lambda x : Factory.ResultPopup(photodir=v).open()) 
       stacklayout.add_widget(imgbtn)
   pass
   
--- Entregable-01/ui/src/core/Recognition.py
+++ Entregable-01/ui/src/core/Recognition.py
@@ -1,7 +1,51 @@
-import time
+import boto3
+import os
+import io
+from PIL import Image, ImageDraw, ExifTags, ImageColor
 
-class Recognition():
-    def process(photo):
-        self.output = ['SAD', 'FURIOUS','HAPPY']
-        time.sleep(2)
-        return self.output
+def ProcessFoo(photodir):
+  photo = os.path.basename(photodir)
+  bucket = 'mybucket677'
+  s3 = boto3.client('s3')
+  s3.upload_file(photodir, bucket,photo )
+
+  client=boto3.client('rekognition')
+
+  s3_connection = boto3.resource('s3')
+  s3_object = s3_connection.Object(bucket,photo)
+  s3_response = s3_object.get()
+
+  stream = io.BytesIO(s3_response['Body'].read())
+
+  response = client.detect_faces(Image={'S3Object': {'Bucket': bucket, 'Name': photo}},
+      Attributes=['ALL'])
+
+  res_arr = []
+  for faceDetail in response['FaceDetails']:
+      item = {}
+      image = Image.open(stream)
+      imgWidth, imgHeight = image.size
+      draw = ImageDraw.Draw(image)
+      box = faceDetail['BoundingBox']
+      left = imgWidth * box['Left']
+      top = imgHeight * box['Top']
+      width = imgWidth * box['Width']
+      height = imgHeight * box['Height']
+
+      points = (
+          (left,top),
+          (left + width, top),
+          (left + width, top + height),
+          (left , top + height),
+          (left, top)
+
+      )
+      draw.line(points, fill='#00d400', width=2)
+      item['image'] = image 
+      item['labels'] = []
+      for emotion in faceDetail['Emotions']:
+          if emotion["Confidence"] > 0:
+              disstr = f'{emotion["Type"]}: {emotion["Confidence"]}' 
+              item['labels'].append(disstr)
+      res_arr.append(item)
+  return res_arr
